643d030ae039d91dec934b30fda81c760d3bbb53
nurmuhammad <96790966+nurmuhammaddeveloper@users.noreply.github.com>
2022-08-22 16:10:27 +0500

    pyTelegramBotAPI

---
+++ main.py
@@ -0,0 +1,26 @@
+from tokenize import Token
+from transliterate import to_cyrillic, to_latin
+import telebot
+TOKEN = '5748814708:AAG6q3-ERB8nhguZI3cEmJxH9OqLI7DzezQ'
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    javob = "Assalomu alaykum xush kelibsiz"
+    javob += "\nMatin kiriting: "
+    bot.reply_to(message, javob)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    msg = message.text
+    javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    bot.reply_to(message, javob(msg))
+bot.polling()
+
+
+
+
+
+
+
+
